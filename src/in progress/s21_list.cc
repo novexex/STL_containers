ce4c0525ac6bee4dba502ceddf84e7d831b5ac06
novexex <nnovexx@gmail.com>
2023-06-05 19:08:04 +0300

    s

--- src/in progress/s21_list.cc
+++ src/in progress/s21_list.cc
@@ -1,2 +1,147 @@
 #include "s21_list.h"
 
+using namespace s21;
+
+template <typename value_type>
+List<value_type>::List() {
+    this->front_ = nullptr;
+    this->rear_ = nullptr;
+    this->size_ = 0;
+}
+
+template <typename value_type>
+List<value_type>::List(size_type n) : List() {
+    for (size_type i = 0; i < n; i++)
+        this->push_back(value_type());
+}
+
+template <typename value_type>
+List<value_type>::List(std::initializer_list<value_type> const &items) : List() {
+    for (auto it = items.begin(); it != items.end(); ++it)
+        this->push(*it);
+}
+
+template <typename value_type>
+List<value_type>::List(const List &other) : List() {
+    this->copy(other);
+}
+
+template <typename value_type>
+List<value_type>::List(List &&other) : List(other) {
+    other.clear();
+}
+
+template <typename value_type>
+List<value_type>::~List() {
+    this->clear();
+}
+
+template <typename value_type>
+List<value_type>& List<value_type>::operator=(List<value_type> &&other) {
+    this->copy(other);
+    return *this;
+}
+
+template <typename value_type>
+typename List<value_type>::const_reference List<value_type>::front() {
+    if (this->front_ == nullptr)
+        throw std::out_of_range("List is empty");
+    return this->front_->data;
+}
+
+template <typename value_type>
+typename List<value_type>::const_reference List<value_type>::back() {
+    if (this->front_ == nullptr)
+        throw std::out_of_range("List is empty");
+    return this->rear_->data;
+}
+
+template <typename value_type>
+typename List<value_type>::iterator begin() {
+    return 
+}
+
+template <typename value_type>
+typename List<value_type>::iterator end() {
+
+}
+
+template <typename value_type>
+bool List<value_type>::empty() {
+
+}
+
+template <typename value_type>
+typename List<value_type>::size_type List<value_type>::size() {
+
+}
+
+template <typename value_type>
+typename List<value_type>::size_type List<value_type>::max_size() {
+
+}
+
+template <typename value_type>
+void List<value_type>::clear() {
+
+}
+
+template<class value_type>
+typename List<value_type>::iterator List<value_type>::insert(iterator pos, const_reference value) {
+
+}
+
+template<class value_type>
+void List<value_type>::erase(iterator pos) {
+
+}
+
+template<class value_type>
+void List<value_type>::push_back(const_reference value) {
+
+}
+
+template<class value_type>
+void List<value_type>::pop_back() {
+
+}
+
+template<class value_type>
+void List<value_type>::push_front(const_reference value) {
+
+}
+
+template<class value_type>
+void List<value_type>::pop_front() {
+
+}
+
+template<class value_type>
+void List<value_type>::swap(List& other) {
+
+}
+
+template<class value_type>
+void List<value_type>::merge(List& other) {
+
+}
+
+template<class value_type>
+void List<value_type>::splice(const_iterator pos, List& other) {
+
+}
+
+template<class value_type>
+void List<value_type>::reverse() {
+
+}
+
+template<class value_type>
+void List<value_type>::unique() {
+
+}
+
+template<class value_type>
+void List<value_type>::sort() {
+
+}
--- src/in progress/s21_list.h
+++ src/in progress/s21_list.h
@@ -5,14 +5,75 @@
 #include <stdexcept>
 
 namespace s21 {
-    template<class T>
+    template <class T>
     class List {
         public:
+            // Member type
+            using value_type      = T;
+            using reference       = T&;
+            using const_reference = const T&;
+            using iterator        = ListIterator<T>;
+            using const_iterator  = ListConstIterator<T>;
+            using size_type       = size_t;
+
+            // Member functions
+            List();
+            List(size_type n);
+            List(std::initializer_list<value_type> const &items);
+            List(const List &l);
+            List(List &&l);
+            ~List();
+            
+            // Operators
+            List<value_type>& operator=(List &&s);
+
+            // Element access
+            const_reference front();
+            const_reference back();
+
+            // Iterators
+            iterator begin();
+            iterator end();
+
+            // Capacity
+            bool empty();
+            size_type size();
+            size_type max_size();
+
+            // Modifiers
+            void clear();
+            iterator insert(iterator pos, const_reference value);
+            void erase(iterator pos);
+            void push_back(const_reference value);
+            void pop_back();
+            void push_front(const_reference value);
+            void pop_front();
+            void swap(List& other);
+            void merge(List& other);
+            void splice(const_iterator pos, List& other);
+            void reverse();
+            void unique();
+            void sort();
+
+            // Convenience methods
 
         private:
-        
-    }
-}
+            class Node {
+                public:
+                    Node *next, *prev;
+                    T data;
+
+                    Node(T data = T(), Node *next = nullptr, Node *prev = nullptr) {
+                        this->next = next;
+                        this->back = prev;
+                        this->data = data;
+                    }
+            };
+
+            Node *top_;
+            size_t size_;
+    };
+}; // namespace s21
 
 #include "s21_list.cc"
 
